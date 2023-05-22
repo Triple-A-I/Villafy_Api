7a6ac2f764069ff8563238fa60201c71bc594e41
albraa abdalla <albraaengineer@gmail.com>
2023-05-22 09:45:36 +0200

    Creating IVillaRepository Interface

---
+++ Repository/IVillaRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+using Villafy_Api.Models;
+
+namespace Villafy_Api.Repository
+{
+    public interface IVillaRepository
+    {
+        Task<List<Villa>> GetAllAsync(Expression<Func<Villa>>? filter = null);
+        Task<Villa> GetByIdAsync(Expression<Func<Villa>>? filter = null, bool tracked = true);
+        Task Create(Villa entity);
+        Task Remove(Villa entity);
+        Task Save();
+    }
+}
