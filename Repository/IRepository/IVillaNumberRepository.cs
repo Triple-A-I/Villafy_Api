1f73bb57c3de48d1968355f09a0f9980af601104
albraa abdalla <albraaengineer@gmail.com>
2023-05-23 17:21:35 +0200

    Creating IVillaNumberRepository and VillaNumberRepository Implementation

---
+++ Repository/IRepository/IVillaNumberRepository.cs
@@ -0,0 +1,10 @@
+﻿using Villafy_Api.Models;
+
+namespace Villafy_Api.Repository.IRepository
+{
+    public interface IVillaNumberRepository : IRepository<VillaNumber>
+    {
+        Task<VillaNumber> UpdateAsync(VillaNumber entity);
+
+    }
+}
---
+++ Repository/VillaNumberRepository.cs
@@ -0,0 +1,25 @@
+﻿using Villafy_Api.Data;
+using Villafy_Api.Models;
+using Villafy_Api.Repository.IRepository;
+
+namespace Villafy_Api.Repository
+{
+    public class VillaNumberRepository : Repository<VillaNumber>, IVillaNumberRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public VillaNumberRepository(ApplicationDbContext db) : base(db)
+        {
+            _dbContext = db;
+        }
+
+        public async Task<VillaNumber> UpdateAsync(VillaNumber entity)
+        {
+            entity.UpdatedDate = DateTime.Now;
+            _dbContext.VillaNumbers.Update(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+    }
+
+}
