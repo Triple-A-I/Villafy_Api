88dde4917f8e2f6f092762f17e842a039c1295ac
albraa abdalla <albraaengineer@gmail.com>
2023-06-11 16:57:36 +0200

    initialize errormessages in APIResponse

--- Models/APIResponse.cs
+++ Models/APIResponse.cs
@@ -4,6 +4,11 @@ namespace Villafy_Api.Models
 {
     public class APIResponse
     {
+
+        public APIResponse()
+        {
+            ErrorMessages = new List<string>();
+        }
         public HttpStatusCode statusCode { get; set; }
         public bool IsSuccess { get; set; } = true;
         public List<string> ErrorMessages { get; set; }
