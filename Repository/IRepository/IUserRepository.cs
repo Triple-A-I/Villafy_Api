f9b73a2422f67f0e0981b8a1e08981ef6ed092b3
albraa abdalla <albraaengineer@gmail.com>
2023-06-11 17:01:50 +0200

    create IUserRepo interface

---
+++ Repository/IRepository/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using Villafy_Api.Models;
+using Villafy_Api.Models.Dto.Auth;
+
+namespace Villafy_Api.Repository.IRepository
+{
+    public interface IUserRepository
+    {
+        bool IsUserUnique(string username);
+        Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
+        Task<LocalUser> Register(RegistrationRequestDto registrationRequestDto);
+    }
+}
