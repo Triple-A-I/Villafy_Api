123d87d39eeb323d1022a7882bebc74f8881fce0
albraa abdalla <albraaengineer@gmail.com>
2023-06-11 16:55:04 +0200

    create villa number dtos

---
+++ Models/Dto/VillaNumber/VillaNumberDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Villafy_Api.Models.Dto.Villa;
+
+namespace Villafy_Api.Models.Dto.VillaNumber
+{
+    public class VillaNumberDto
+    {
+        public int VillaNo { get; set; }
+        public string SpecialDetails { get; set; }
+        [Required]
+
+        public int VillaId { get; set; }
+        public VillaDto Villa { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
---
+++ Models/Dto/VillaNumber/VillaNumberUpdateDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Villafy_Api.Models.Dto.VillaNumber
+{
+    public class VillaNumberUpdateDto
+    {
+        [Required]
+        public int VillaNo { get; set; }
+        public string SpecialDetails { get; set; }
+        [Required]
+
+        public int VillaId { get; set; }
+
+    }
+}
