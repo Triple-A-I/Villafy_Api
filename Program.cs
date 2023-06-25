544b0e0a1c0e815949ffbe8c726790b92b0f2810
albraa abdalla <albraaengineer@gmail.com>
2023-06-25 10:54:36 +0200

    add cahing and profile caching

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
@@ -13,6 +14,10 @@ var builder = WebApplication.CreateBuilder(args);
 ///AppDbContext
 var connectionString = builder.Configuration.GetConnectionString("DefaultSQLConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+
+///Caching
+
+builder.Services.AddResponseCaching();
 // Add services to the container.
 
 ///Add IVillaRepository
@@ -47,7 +52,12 @@ builder.Services.AddAuthentication(
 ///// Default log implementation
 //Log.Logger = new LoggerConfiguration().MinimumLevel.Debug().WriteTo.File("Log/villaLogs.txt", rollingInterval: RollingInterval.Day).CreateLogger();
 //builder.Host.UseSerilog();
-builder.Services.AddControllers().AddNewtonsoftJson();
+builder.Services.AddControllers(option =>
+{
+    option.CacheProfiles.Add("Default30",
+        new CacheProfile() { Duration = 30 }
+        );
+}).AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
